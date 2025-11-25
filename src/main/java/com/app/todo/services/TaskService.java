29d33c7849254779590e6091dff71e28f372a6a1
Marius Gheorghe <MariusGeorgian.Gheorghe@yahoo.com>
2025-11-25 17:20:01 +0200

    Add files via upload

---
+++ src/main/java/com/app/todo/services/TaskService.java
@@ -0,0 +1,38 @@
+package com.app.todo.services;
+
+import com.app.todo.models.Task;
+import com.app.todo.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) throws IllegalAccessException {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new IllegalAccessException("Invalid task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+    }
+}
